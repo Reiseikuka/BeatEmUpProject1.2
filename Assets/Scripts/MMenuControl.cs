3573334c76e7585b6635e8de1e093152672146f5
Reiseikuka <reiseirakuka@outlook.com>
2023-05-23 18:55:21 -0600

    master volume p1

--- Assets/Scripts/MMenuControl.cs
+++ Assets/Scripts/MMenuControl.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 namespace SA
 {
     public class MMenuControl : MonoBehaviour
     {
+        [Header("Volume Settings")]
+        [SerializeField] private TMP_Text volumeTextValue = null;
+        [SerializeField] private Slider volumeSlider = null;
+
+        [SerializeField] private GameObject confirmationPrompt = null;
+
+
+
         [Header("Levels To Load")]
         public string _newGameLevel;
         //Load or Run at any point when we create a New Game
@@ -41,6 +50,25 @@ namespace SA
         public void ExitButton()
         {
             Application.Quit();
+        }
+
+        public void SetVolume(float volume)
+        {
+            AudioListener.volume = volume;
+            volumeTextValue.text = volume.ToString("0.0");
+        }
+
+        public void VolumeApply()
+        {
+            PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+            StartCoroutine(ConfirmationBox());
+        }
+
+        public IEnumerator ConfirmationBox()
+        {
+            confirmationPrompt.SetActive(true);
+            yield return new WaitForSeconds(2);
+            confirmationPrompt.SetActive(false);
         }
         
     }
