58aceb3ba2757dea8264224c137b71bfdc5639dd
Reiseikuka <reiseirakuka@outlook.com>
2023-05-23 19:34:11 -0600

    master volume

--- Assets/Scripts/MMenuControl.cs
+++ Assets/Scripts/MMenuControl.cs
@@ -12,9 +12,11 @@ namespace SA
         [Header("Volume Settings")]
         [SerializeField] private TMP_Text volumeTextValue = null;
         [SerializeField] private Slider volumeSlider = null;
+        [SerializeField] private float defaultVolume = 1.0f;
 
+
+        [Header("Confirmation")]
         [SerializeField] private GameObject confirmationPrompt = null;
-
 
 
         [Header("Levels To Load")]
@@ -23,6 +25,7 @@ namespace SA
         private string levelToLoad;
         //Will Load our level when we need it
         [SerializeField] private GameObject noSavedGameDialog = null;
+
 
 
         public void NewGameDialogYes()
@@ -62,6 +65,17 @@ namespace SA
         {
             PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
             StartCoroutine(ConfirmationBox());
+        }
+
+        public void ResetButton(string MenuType)
+        {
+            if (MenuType == "Audio")
+            {
+                AudioListener.volume = defaultVolume;
+                volumeSlider.value = defaultVolume;
+                volumeTextValue.text = defaultVolume.ToString("0.0");
+                VolumeApply();
+            }
         }
 
         public IEnumerator ConfirmationBox()
