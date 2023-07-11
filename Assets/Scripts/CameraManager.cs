01b7f4b079a9e97c12cc3f49f18b4d02819b97da
Reiseikuka <reiseirakuka@outlook.com>
2023-07-11 16:54:49 -0600

    Camera on Player

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -12,9 +12,10 @@ namespace SA
         //Waypoint 1
         public Transform p2;
         //Waypoint 2
+
         private void Update()
         {
-            Vector3 p = FindNearestPointOnLine(p1.position,p2.position, transform.position);
+            Vector3 p = FindNearestPointOnLine(target.position, p1.position,p2.position);
             p.z = transform.position.z;
             transform.position = p;
 
@@ -24,6 +25,23 @@ namespace SA
             //transform.position = tp;
         }
 
+
+        // For finite lines:
+        Vector3 GetClosestPointOnFiniteLine(Vector3 point, Vector3 line_start, Vector3 line_end)
+        {
+            Vector3 line_direction = line_end - line_start;
+            float line_length = line_direction.magnitude;
+            line_direction.Normalize();
+            float project_length = Mathf.Clamp(Vector3.Dot(point - line_start, line_direction), 0f, line_length);
+            return line_start + line_direction * project_length;
+        }
+
+
+        // For infinite lines:
+        Vector3 GetClosestPointOnInfiniteLine(Vector3 point, Vector3 line_start, Vector3 line_end)
+        {
+            return line_start + Vector3.Project(point - line_start, line_end - line_start);
+        }
         public Vector2 FindNearestPointOnLine(Vector2 origin, Vector2 end, Vector2 point)
         {
             //Get heading
@@ -38,5 +56,6 @@ namespace SA
             return origin + heading * dotP;
         }
     }
+    
 }
 
